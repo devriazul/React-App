fc47fe36da75481dca291ae73ba817dc011f13bd
devriazul <riazul56@gmail.com>
2021-08-30 03:35:49 +0600

    Functional components to Class components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Components/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <Person />
-    </div>
-  );
-  // return React.createElement('div', { className: "App" }, React.createElement('h1', null, "Hello World!"), <Person />);
-}
-function Person() {
-  return (
-    <div>
-      <h1>Person component</h1>
-    </div>
-  );
-}
-// return React.createElement('div', null, React.createElement('h1', null, "I am person component"));
+//Functional Components
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World!</h1>
+//       <Person />
+//     </div>
+//   );
+
+// }
+
+//Class components
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <Person />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Components/Person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Person() {
+    return (
+        <div>
+            <h1>Person component</h1>
+        </div>
+    );
+}
+
+export default Person;
